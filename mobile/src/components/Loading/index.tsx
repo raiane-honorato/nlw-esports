eb07d6db2ca7d6bf72d9f332a980932afb37f4be
raiane-honorato <raianehonorato16@gmail.com>
2022-09-14 17:14:06 -0300

    feat(create mobile interface

---
+++ mobile/src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+import { THEME } from '../../theme';
+import { View, ActivityIndicator } from 'react-native';
+import { styles } from './styles';
+
+export function Loading() {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator color={THEME.COLORS.PRIMARY}/>
+    </View>
+  );
+}
